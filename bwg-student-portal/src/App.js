832bb3cdc0c5792af2687457f08a68b8a031c9d1
Mathew <mathewharvey@gmail.com>
2021-01-29 21:14:15 +0800

    added firebase login

--- bwg-student-portal/src/App.js
+++ bwg-student-portal/src/App.js
@@ -1,25 +1,131 @@
 
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import Main from "./components/main/Main";
-import Navbar from "./components/navbar/Navbar";
-import Sidebar from "./components/sidebar/Sidebar";
+import fire from './fire'
+import Login from './login'
+import Hero from "./hero"
+
 
 const App = () => {
-  const [sidebarOpen, setsidebarOpen] = useState(false);
-  const openSidebar = () => {
-    setsidebarOpen(true);
+
+  //LOGIN STUFF
+  const [user, setUser] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [hasAccount, setHasAccount] = useState(false);
+
+  const clearInputs = () => {
+    setEmail('');
+    setPassword('');
+  }
+
+  const clearErrors = () => {
+    setEmailError('');
+    setPasswordError('');
+  }
+
+  const handleLogin = () => {
+    clearErrors();
+    fire
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch(err => {
+        switch (err.code) {
+          case "auth/invalid-email":
+          case "auth/user-disabled":
+          case "auth/user-not-found":
+            setEmailError(err.message);
+            break;
+          case "auth/wrong-password":
+            setPasswordError(err.message);
+            break;
+        }
+      });
   };
-  const closeSidebar = () => {
-    setsidebarOpen(false);
+
+  const handleSignup = () => {
+    clearErrors();
+    fire
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch(err => {
+        switch (err.code) {
+          case "auth/email-already-in-use":
+          case "auth/invalid-email":
+            setEmailError(err.message);
+            break;
+          case "auth/weak-password":
+            setPasswordError(err.message);
+            break;
+        }
+      });
   };
+
+  const handleLogout = () => {
+    fire.auth().signOut();
+  };
+
+  const authListener = () => {
+    fire.auth().onAuthStateChanged(user => {
+      if (user) {
+        clearInputs();
+        setUser(user);
+      } else {
+        setUser("")
+      }
+    });
+  };
+
+  useEffect(() => {
+    authListener();
+  }, [])
+
+
+  //login screen
+
   return (
-    <div className="container">
-      <Navbar sidebarOpen={sidebarOpen} openSidebar={openSidebar} />
-      <Main />
-      <Sidebar sidebarOpen={sidebarOpen} closeSidebar={closeSidebar} />
+    <div className="App">
+      {user ? ( //if user exists render document 'hero' replace with dash board
+        <Hero handleLogout={handleLogout} />
+      ) : ( // otherwise if use doesn't exist render log in page
+          <Login
+            email={email}
+            setEmail={setEmail}
+            password={password}
+            setPassword={setPassword}
+            handleLogin={handleLogin}
+            handleSignup={handleSignup}
+            hasAccount={hasAccount}
+            setHasAccount={setHasAccount}
+            emailError={emailError}
+            passwordError={passwordError}
+          />
+        )}
     </div>
-  );
+  )
+
+
+
+
+
+
+  // //Dashboard Stuff
+  // const [sidebarOpen, setsidebarOpen] = useState(false);
+  // const openSidebar = () => {
+  //   setsidebarOpen(true);
+  // };
+  // const closeSidebar = () => {
+  //   setsidebarOpen(false);
+  // };
+  // return (
+  //   <div className="container">
+  //     <Navbar sidebarOpen={sidebarOpen} openSidebar={openSidebar} />
+  //     <Main />
+  //     <Sidebar sidebarOpen={sidebarOpen} closeSidebar={closeSidebar} />
+  //   </div>
+  // );
 };
 
 export default App;
---
+++ bwg-student-portal/src/fire.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBHIdj6KPxrftUrQ-_QcsNgClXBdokAzVg",
+    authDomain: "bwg-student-portal-dev.firebaseapp.com",
+    projectId: "bwg-student-portal-dev",
+    storageBucket: "bwg-student-portal-dev.appspot.com",
+    messagingSenderId: "912139319203",
+    appId: "1:912139319203:web:5d6cec0845ac1c32be4c16"
+  };
+  // Initialize Firebase
+  const fire= firebase.initializeApp(firebaseConfig);
+
+  export default fire;
