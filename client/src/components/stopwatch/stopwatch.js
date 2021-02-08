ea3e0988edb3b0a99a468cdabdac95d7e65a0f20
mat <mathewharvey@gmail.com>
2021-02-08 21:32:34 +0800

    timer added

--- client/src/components/stopwatch/stopwatch.js
+++ client/src/components/stopwatch/stopwatch.js
@@ -1,20 +1,54 @@
 import React, {useState} from 'react';
-import ReactTimerStopwatch from 'react-stopwatch-timer';
+import Timer from 'react-compound-timer';
 import "./stopwatch.css"
  
-const fromTime = new Date(0, 0, 0, 0, 10, 0, 0);
+
+
+
  
 const Stopwatch = () => {
- 
-   const [isOn, setIsOn] = useState(false);
+
+
+
  
    return (
  <div>
-       <ReactTimerStopwatch isOn={isOn} className="react-stopwatch-timer__table" watchType="timer"
-                            displayCircle={true} color="gray" hintColor="red" fromTime={fromTime} displayHours={false}>
-           <button onClick={() => setIsOn(true) }>START</button>
-           <button onClick={() => setIsOn(false) }>STOP</button>
-       </ReactTimerStopwatch>
+<Timer
+    initialTime={0}
+    startImmediately={false}
+    direction="forward"
+    checkpoints={[
+      {
+          time: 100,
+          callback: () => console.log('Checkpoint A'),
+      },
+      {
+          time: 60000 * 60 * 48 - 5000,
+          callback: () => console.log('Checkpoint B'),
+      }
+  ]}
+>
+    {({ start, resume, pause, stop, reset, timerState }) => (
+        <React.Fragment>
+            <div>
+
+                <Timer.Minutes /> min :
+                <Timer.Seconds /> sec
+              
+            </div>
+            {/* <div>{timerState}</div> */}
+            <br />
+            <div>
+                <button onClick={start}>Start</button>
+                <button onClick={pause}>Pause</button>
+                <button onClick={resume}>Resume</button>
+                <button onClick={stop}>Stop</button>
+                <button onClick={reset}>Reset</button>
+            </div>
+        </React.Fragment>
+    )}
+</Timer>
+      
        </div>
 
    );
